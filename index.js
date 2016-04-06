745aa3d6e0af5933429d349b3ab6a2ff7f8d715b
Michael Lyons <mdl0394@gmail.com>
2016-04-05 22:23:54 -0700

    Initial commit, broke away from Dor source

---
+++ index.js
@@ -0,0 +1,131 @@
+/**
+ * Copyright (C) 2016 Dor Technologies
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+var child_process = require( 'child_process' ),
+    path = require( 'path' ),
+    q = require( 'q' ),
+    _ = require( 'underscore' );
+
+var request = require( 'request' ),
+    juice = require( 'juice' );
+
+var PHANTOM_SERVER_SCRIPT_NAME = 'phantom-server.js';
+var PHANTOM_SERVER_HOSTING_MESSAGE = 'hosting\n';
+var PHANTOM_SERVER_PORT = 4321;
+
+var PhantomServer = function() {
+    this._port = PHANTOM_SERVER_PORT; // Increment so that future servers have different ports
+    this._phantom_server_promise = null;
+    this._phantom_child_process = null;
+
+    this._getPhantomJSPath = function( path ) {
+        if( path ) return path;
+        return require( 'phantomjs' ).path;
+    }
+
+    /**
+     * Returns the arguments for the phantomjs process to be ran with
+     * @return {Array} Array of arguments to be passed to child_process.spawn
+     */
+    this._getPhantomJSArguments = function() {
+        return [
+            path.join( __dirname, PHANTOM_SERVER_SCRIPT_NAME ),
+            this._port
+        ];
+    };
+
+    this.start = function( phantomjs_path ) {
+        var _this = this;
+
+        if( _this._phantom_server_promise !== null ) return _this._phantom_server_promise;
+
+        _this._phantom_server_promise = q
+            .Promise( function( resolve, reject ) {
+                phantomjs_path = _this._getPhantomJSPath( phantomjs_path );
+
+                _this._phantom_child_process = child_process
+                    .spawn( phantomjs_path, _this._getPhantomJSArguments(), {
+                        cwd: __dirname
+                    } )
+
+                _this._phantom_child_process.stdout
+                    .on( 'data', function( msg ) {
+                        console.log( msg.toString() );
+                        if( msg.toString() === PHANTOM_SERVER_HOSTING_MESSAGE ) {
+                            resolve( _this );
+                        }
+                    } );
+
+                _this._phantom_child_process.stderr
+                    .on( 'data', function( err ) {
+                        console.error( err.toString() );
+                        reject( err.toString() );
+                    } );
+            } );
+
+        return _this._phantom_server_promise;
+    };
+
+    this.stop = function() {
+        this._phantom_child_process.kill();
+        this._phantom_child_process = null;
+        this._phantom_server_promise = null;
+    };
+
+    this._requestServer = function( data ) {
+        var url = 'http://localhost:' + this._port;
+
+        console.log( 'sending', data );
+
+        return q
+            .Promise( function( resolve, reject ) {
+                request( {
+                    url: url,
+                    method: 'POST',
+                    body: data,
+                    json: true
+                }, function( err, res, body ) {
+                    if( err ) return reject( err );
+                    if( res.statusCode !== 200 ) return reject( res.body );
+                    resolve( body );
+                } );
+            } );
+    };
+
+    /**
+     * Runs the given scripts in PhantomJS
+     *
+     * Will wait for server to start. If not started it will attempt to start it
+     *
+     * @param  {Object} options Pass information about what should be run here
+     * @param  {Array=[]} options.inject_scripts Scripts to be injected on the page, using page.injectJs
+     * @param  {Array=[]} options.evaluate_scripts Scripts to be evaluated on the page, using page.evaluate
+     * @return {q:Promise}         A promise that will return the results of the evaluated scripts
+     */
+    this.run = function( options ) {
+        var _this = this;
+
+        options = options || {};
+        options = _
+            .defaults( options, {
+                inject_scripts: [],
+                evaluate_scripts: [],
+                data: {},
+                phantomjs_path: null
+            } );
+
+        return _this
+            .start( options.phantomjs_path )
+            .then( function() {
+                console.log( 'requesting' );
+                return _this
+                    ._requestServer( options );
+            } );
+    };
+};
+
+module.exports = PhantomServer;
---
+++ phantom-server.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2016 Dor Technologies
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+var server = require( 'webserver' ).create(),
+    fs = require( 'fs' ),
+    system = require( 'system' ),
+    page = require( 'webpage' ).create();
+
+var page_path = fs.absolute( 'phantom.html' );
+
+/**
+ * Parses the payload on the request, and returns it as an Object
+ * @param  {Object} req The request object from our phantomjs server listener
+ * @return {Object}     If parse success then object, otherwise null
+ */
+var parsePayload = function( req ) {
+    try {
+        return JSON.parse( req.post );
+    } catch( e ) {
+        return null;
+    }
+};
+
+/**
+ * Responds from the server with an error
+ * @param  {Object} res The response object for the phantomjs server
+ */
+var sendError = function( res, code, body ) {
+    res.statusCode = code;
+    res.write( body );
+    res.close();
+};
+
+var service = server.listen( 4321 || +system.args[ 1 ], function( req, res ) {
+    console.log( 'phantom-server pre-parse', JSON.stringify( req ) );
+    var payload = parsePayload( req );
+
+    if( !payload || !payload.data || !payload.evaluate_scripts || !payload.inject_scripts ) return sendError( res, 400, 'Missing required keys' );
+
+    page.open( page_path, function( status ) {
+        if( status !== 'success' ) return sendError( res, 500, 'Failed to load page, status: ' + status );
+
+        for( var inject_script_i in payload.inject_scripts ) {
+            var script_path = payload.inject_scripts[ inject_script_i ];
+            console.log( 'Injecting ->', script_path );
+            page.injectJs( script_path );
+        }
+
+        var evaluate_responses = payload.evaluate_scripts
+            .map( function( script_path ) {
+                console.log( 'Evaluating ->', script_path );
+                var loaded_script = require( script_path );
+                var response = page.evaluate( loaded_script, payload.data );
+                return response;
+            } );
+
+        console.log( evaluate_responses );
+        console.log( JSON.stringify( evaluate_responses ) );
+
+        res.statusCode = 200;
+        // res.setHeader( 'Content-Type', 'image/svg+xml' );
+        res.write( JSON.stringify( evaluate_responses ) );
+        res.close();
+    } );
+} );
+
+if( service ) {
+    console.log( 'hosting' );
+}
